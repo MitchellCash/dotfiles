a37ee80179c4d90538432c61a4217366ff74d24e
Mitchell Cash <mitchell@mitchellcash.com>
2019-12-04 12:28:14 +0000

    Clean up dotfiles script

--- script/dotfiles.sh
+++ script/dotfiles.sh
@@ -10,38 +10,36 @@ log_info "Setting up dotfiles"
 # reason I don't store them in the git repo with the dot is so that it is easier
 # to manage repo specific dotfiles like .gitignore etc.
 install_dotfiles() {
-    cd .dotfiles || exit
-    for file in {zprofile,zshrc,gitconfig,gitignore,hushlogin}; do
-        if [ -r "$file" ] && [ -f "$file" ]; then
-            rsync -avh --no-perms $file ~/.$file
-        fi
-    done
-    cd .. || exit
+  log_info "Installing dotfiles to root..."
 
-    for folder in {gnupg,ssh,terminal-theme}; do
-        if [ -d ".$folder" ]; then
-            rsync -avh --no-perms .$folder/ ~/.$folder
-        fi
-    done
+  cd .dotfiles || exit
+  for file in {zprofile,zshrc,gitconfig,gitignore,hushlogin}; do
+    if [ -r "$file" ] && [ -f "$file" ]; then
+      rsync -avh --no-perms $file ~/.$file
+    fi
+  done
+  cd .. || exit
+
+  for folder in {gnupg,ssh,terminal-theme}; do
+    if [ -d ".$folder" ]; then
+      rsync -avh --no-perms .$folder/ ~/.$folder
+    fi
+  done
+
+  log_success "Dotfiles successfully installed!"
 }
 
 # Confirm with the user that proceeding to install the dotfiles can be
 # destructive.
-if [[ ${FORCE} -eq 1 ]]; then
-    log_info "Installing dotfiles to ~"
-    install_dotfiles
-    log_success "dotfiles successfully installed!"
-else
-    log_info "Installing dotfiles to ~ may overwrite existing files in your home directory. Are you sure? [y/N]"
-    read -r
+if [[ ${FORCE} -ne 1 ]]; then
+  log_info "Installing dotfiles to root may overwrite existing files in your home directory. Are you sure? [y/N]"
+  read -r
+fi
 
-    if [[ $REPLY =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-      log_info "Proceeding with installing dotfiles to ~"
-      install_dotfiles
-      log_success "dotfiles successfully installed!"
-    else
-      log_warn "Skipping installing dotfiles"
-    fi
+if [[ $REPLY =~ ^([yY][eE][sS]|[yY])+$ || ${FORCE} -eq 1 ]]; then
+    install_dotfiles
+else
+  log_warn "Skipping installing dotfiles!"
 fi
 
 unset install_dotfiles
